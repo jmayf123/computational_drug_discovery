492569c9beb4948b05d4c2a25b2249a53b2729cf
Jackson Mayfield <jacksonm.tn@gmail.com>
2023-01-05 17:48:13 -0600

    Data cleaned, made new conda env .yml file

--- shiny_app/python_notebooks/functions.py
+++ shiny_app/python_notebooks/functions.py
@@ -6,15 +6,23 @@ from chembl_webresource_client.new_client import new_client
 def target_search(target_name):
     target = new_client.target
     res = target.filter(pref_name__icontains = target_name)
-    targets = pd.DataFrame(res)
-    return(targets) #Returns a df of targets matching your search term
+    if len(res) > 200:
+        raise ValueError('Search Target too broad, Try Another Search Term.')
+    else:
+        targets = pd.DataFrame(res)
+        return(targets) #Returns a df of targets matching your search term
 
 #Bioactivty data after getting target selection   
 def get_bioactivities_data(target_chembl_ID):
     activity = new_client.activity
-    res = activity.filter(target_chembl_id=target_chembl_ID).filter(standard_type="IC50")
-    df = pd.DataFrame(res)
-    if df.size == 0:
-        return('Error, no bioactivities for chosen target.')
-    else:
-        return(df)
+    res = activity.filter(target_chembl_id=target_chembl_ID).filter(standard_type="IC50").only(['molecule_chembl_id','canonical_smiles','standard_value', 'standard_type'])
+    df = pd.DataFrame(res[0:500])#Set a limit for now, take too long to compute over 500 activities
+    df['standard_value'] = df['standard_value'].astype(float)
+    
+    conditions = [
+        (df['standard_value'] >= 10000),
+        (df['standard_value'] <= 1000)
+    ]
+    choices = ['inactive', 'active']
+    df['bioactivity_class'] = np.select(conditions, choices, default = 'intermediate')
+    return(df)
